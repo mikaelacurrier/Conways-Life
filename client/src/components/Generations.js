bfded6a0e3c4dab3b72a562ca0c205298b87f21c
mikaelacurrier <mikaela.currier@gmail.com>
2019-07-17 16:30:49 -0400

    added header and generation counter

---
+++ client/src/components/Generations.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Generations = props => {
+	return <div>Generations: {props.generations}</div>;
+};
+
+export default Generations;
---
+++ client/src/components/TopBar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TopBar = () => {
+	return <h1>Conway's Game of Life</h1>;
+};
+
+export default TopBar;
